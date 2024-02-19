318d992b609cac08fdb57337d988c58067225f14
Ankit-euphemism <153090276+Ankit-euphemism@users.noreply.github.com>
2024-02-19 22:38:03 +0530

    Add files via upload

---
+++ Datatypes.py
@@ -0,0 +1,26 @@
+#String
+c="Hello World"
+print(c)
+#integer-my whole number
+a=596
+print(a)
+#float-Decimal value
+b=45.45
+print(b)
+#List
+print([10,"Data"])
+#hold multiple elements of different datatypes
+
+#dictionary
+print({1:"Ak",2:"Class"})
+#1 is key and Ak is values
+
+#tuple-immutable(can't be changed)
+print((1,2,3,5))
+
+#set-do not have duplicate value
+print({1,1,2,4,7})
+
+#boolean
+print(5<4)#False
+
