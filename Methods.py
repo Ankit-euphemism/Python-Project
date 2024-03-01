e535ea86f4af2f83ddba0c4132b0f521fe9665d1
Ankit-euphemism <153090276+Ankit-euphemism@users.noreply.github.com>
2024-03-01 10:54:57 +0530

    Update Methods.py

--- Methods.py
+++ Methods.py
@@ -1,5 +1,5 @@
 #String Methods
-S="ankit kumar"
+S="ankit kumar {Age}"
 P=" mihir dutta"
 print(S.capitalize())#Capitalize first Letter
 print(S.count("a"))#Count nubmer of occurence
@@ -14,6 +14,8 @@ print(P.startswith("k"))#Check The Stsrting of string with particular character 
 print(S.replace("a","A"))#Replace the character from new Character
 print(P.title())#Capitalize first letter of each word in string
 print(S.split())#Make the list of the String
+print(S.center(20))#Align the return String in center
+print(S.format(Age=19))#Add the value in the string
 
 #List Methods
 li=["a","b","c"]
@@ -28,3 +30,23 @@ li.insert(1,"z")#Insert the value in between
 print(li)
 li.pop()#Removes Last Element
 print(li)
+print(len(li))#Checks the length of the function
+
+#set methods
+A={"a","b","c"}
+B={"c","e","f"}
+A.add("d")#add the value in the set
+print(A)
+A.remove("b") #remove the element from the set
+print(A)
+print(A.union(B)) #Union two set
+print(A.intersection(B)) #Find common value from two sets
+
+#Dictionary Methods
+C={1:"Ankit",2:"Mine"}
+print(C.keys()) #print all the keys
+print(C.values()) #print all the values
+print(C.items()) #items shows in the form of list
+print(C.get(2)) #Get the value of any key
+C.update({3:"Anyone"})#Add new element in a dictionary
+print(C)
